da0fad297e96dfb9544a72a12429593c5f397525
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-02-26 20:21:06 +0200

    Reworked some stuff in the board service. Created a createBoard, which temporarily acts as a way for me to push board data to Firestore.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BoardListComponent } from './kanban/board-list/board-list.component';
 import { AuthGuard } from './user/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: HomeComponent, data: {openAuthDrawer: true}},
-  { path: 'kanban', 
-    component: HomeComponent, 
+  { path: 'kanban',
+    component: BoardListComponent, 
     loadChildren: () => import("./kanban/kanban.module").then((m) => m.KanbanModule),
     canActivate: [AuthGuard],
   },
--- src/app/kanban/board.service.ts
+++ src/app/kanban/board.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Board, Task } from './board.model';
 import firebase from "firebase/compat/app";
 import { switchMap } from 'rxjs';
@@ -9,30 +9,39 @@ import { switchMap } from 'rxjs';
   providedIn: 'root'
 })
 export class BoardService {
+  private _boardsCollection: AngularFirestoreCollection<Board>;
 
   constructor(
     private afAuth: AngularFireAuth,
     private db: AngularFirestore
-  ) {}
+  ) {
+    this._boardsCollection = this.db.collection("boards");
+  }
 
   async createBoard(data: Board) {
-    // const user = await this.afAuth.currentUser;
-    return this.db.collection("boards").add({
-      ...data
-    })
+    const user = await this.afAuth.currentUser;
+    if (user) {
+      return this._boardsCollection.add({
+        ...data,
+        userUID: user.uid,
+      })
+    } else {
+      console.error("User not logged in!");
+      return;
+    }
   }
 
   deleteBoard(boardID: string) {
-    return this.db.collection("boards").doc(boardID).delete();
+    return this._boardsCollection.doc(boardID).delete();
   }
 
   updateTasks(boardID: string, tasks: Task[]) {
-    return this.db.collection("boards").doc(boardID).update({ tasks })
+    return this._boardsCollection.doc(boardID).update({ tasks })
   }
 
   removeTask(boardID: string, task: Task) {
-    return this.db.collection("boards").doc(boardID).update({
-      tasks:  firebase.firestore.FieldValue.arrayRemove(task)
+    return this._boardsCollection.doc(boardID).ref.update({
+      tasks: firebase.firestore.FieldValue.arrayRemove(task)
     });
   }
 
@@ -40,8 +49,8 @@ export class BoardService {
     return this.afAuth.authState.pipe(
       switchMap((user) => {
         if (user) {
-          return this.db.collection("boards", ref => ref.where("uid", "==", user.uid).orderBy("priority"))
-          .valueChanges({ idField: "id" });
+          const boardsQuery = this._boardsCollection.ref.where("userUID", "==", user.uid).orderBy("priority");
+          return boardsQuery.get();
         }
         return [];
       })
@@ -51,8 +60,8 @@ export class BoardService {
   async sortBoards(boards: Board[]) {
     const db = firebase.firestore();
     const batch = db.batch();
-    const refs = boards.map((board) => db.collection("boards").doc(board.id));
-    refs.forEach((ref, idx) => batch.update(ref, { priority: idx}));
+    const currentBoardsFromStorage = boards.map((board) => this._boardsCollection.doc(board.id));
+    currentBoardsFromStorage.forEach((board, idx) => batch.update(board.ref, { priority: idx}));
 
     try {
       await batch.commit();
