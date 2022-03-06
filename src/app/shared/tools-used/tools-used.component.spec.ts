525bbab832723b5722729af9d1428334334fa54a
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-03-06 02:59:48 +0200

    This push was related to the previous commit. For somehow this change was left out from it

---
+++ src/app/shared/tools-used/tools-used.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ToolsUsedComponent } from './tools-used.component';
+
+describe('ToolsUsedComponent', () => {
+  let component: ToolsUsedComponent;
+  let fixture: ComponentFixture<ToolsUsedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ToolsUsedComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ToolsUsedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/tools-used/tools-used.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-source-code',
+  templateUrl: './tools-used.component.html',
+  styleUrls: ['./tools-used.component.scss']
+})
+export class ToolsUsedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
